ea903d87a730e425580867ba187e95cbaee95937
Malibin <nightmare73@naver.com>
2020-11-25 18:27:13 +0900

    [Feat] View 작성

--- app/src/main/java/com/malibin/stopwatch/StopWatchActivity.kt
+++ app/src/main/java/com/malibin/stopwatch/StopWatchActivity.kt
@@ -2,13 +2,13 @@ package com.malibin.stopwatch
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.malibin.stopwatch.databinding.ActivityMainBinding
+import com.malibin.stopwatch.databinding.ActivityStopWatchBinding
 
-class MainActivity : AppCompatActivity() {
+class StopWatchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        val binding = ActivityStopWatchBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 }
