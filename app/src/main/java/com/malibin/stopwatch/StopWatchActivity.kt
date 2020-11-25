c561934ab1ef10aefd4c620ee94b4d3e83205b5b
Malibin <nightmare73@naver.com>
2020-11-25 21:18:11 +0900

    [Feat] 가로모드 뷰 따로 작성 및 디자인 작업 완료

--- app/src/main/java/com/malibin/stopwatch/StopWatchActivity.kt
+++ app/src/main/java/com/malibin/stopwatch/StopWatchActivity.kt
@@ -1,9 +1,11 @@
 package com.malibin.stopwatch
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.malibin.stopwatch.databinding.ActivityStopWatchBinding
+import com.malibin.stopwatch.databinding.ActivityStopWatchLandBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -13,11 +15,21 @@ class StopWatchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        initView()
+
+        val orientation = resources.configuration.orientation
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) intLandScapeView()
+        else initPortraitView()
     }
 
-    private fun initView() {
+    private fun initPortraitView() {
         val binding = ActivityStopWatchBinding.inflate(layoutInflater)
+        binding.viewModel = stopWatchViewModel
+        binding.lifecycleOwner = this
+        setContentView(binding.root)
+    }
+
+    private fun intLandScapeView() {
+        val binding = ActivityStopWatchLandBinding.inflate(layoutInflater)
         binding.viewModel = stopWatchViewModel
         binding.lifecycleOwner = this
         setContentView(binding.root)
